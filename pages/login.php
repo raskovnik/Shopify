48cfe0517c6c005bfed4e2c815df81043026c87e
Anthony K. Mwangi <68157384+raskovnik@users.noreply.github.com>
2023-01-10 20:11:46 +0300

    login and register backend

--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+    include "../scripts/connect.php";
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -12,16 +16,32 @@
         <section>
             <div class="login">
             <h2>Log in</h2> 
-            <form class="n">
+            <form class="n" method="POST" action='login.php'>
                 <label for="email"><h4>Enter your email address</h4></label><br>
                 <input type="text" id="email" name="email"  style="width:69.5%;height: 30px;border-radius:10px;border-color: blue;" ><br>
                 <label for="pwd"><h4>Password</h4></label><br>
-                <input type="password" id="pwd" name="pwd" style="width:69.5%;height: 30px;border-radius:10px;border-color: blue;"<br>
+                <input type="password" id="pwd" name="pwd" style="width:69.5%;height: 30px;border-radius:10px;border-color: blue;"><br><br>
+                <input type="submit" value="submit" name="submit"><br><br>
             </form>
-            <button type="button" class="button1"><a href="index.php">Log in</a></button><br>
             <a href="reg.php">Register</a><br>
             <a href="forgot.php" target="_blank">Forgot password</a><br>
             <p id="cr">&#169 2022</p>
+            <?php
+                if (isset($_POST["submit"])) {
+                    $email = $_POST["email"];
+                    $password = $_POST["pwd"];
+
+                $sql = "SELECT * FROM `users` WHERE `email`='$email' AND `password`='$password'";
+                if (mysqli_num_rows(mysqli_query($conn, $sql)) != 1) {
+                    echo "Input correct credentials";
+                } else {
+                    $_SESSION["username"] = $username;
+                    $_session["email"] = $email;
+                    $_SESSION["role"] = $role;
+                    header("location:../pages/index.php");
+                }
+                }
+            ?>
         </section>
         </article>
         </div>
